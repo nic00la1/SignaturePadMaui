ce96f6732ee134b3dbf55b71fc4b680e6335bb90
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-29 16:59:40 +0100

    Binding and Saving Lines ✅

--- SignaturePadMaui/MainPage.xaml.cs
+++ SignaturePadMaui/MainPage.xaml.cs
@@ -1,13 +1,22 @@
-﻿namespace SignaturePadMaui
+﻿using CommunityToolkit.Maui.Core;
+using System.Collections.ObjectModel;
+
+namespace SignaturePadMaui
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<IDrawingLine> Lines { get; set; } = new ObservableCollection<IDrawingLine>();
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
