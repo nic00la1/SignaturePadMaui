a52cffd551619a2b057283b847e5c0c9bda4946b
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-29 16:27:14 +0100

    Add project files.

---
+++ SignaturePadMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SignaturePadMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SignaturePadMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SignaturePadMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
