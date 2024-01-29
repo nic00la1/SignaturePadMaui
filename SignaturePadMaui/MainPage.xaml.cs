c652b5ad2188d5c306e16bfe13c740678415fd97
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-29 17:18:59 +0100

    Saving Drawing to a Stream ✅✨ and Saving multiple lines to a stream ✨✅

--- SignaturePadMaui/MainPage.xaml.cs
+++ SignaturePadMaui/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Views;
 using System.Collections.ObjectModel;
 
 namespace SignaturePadMaui
@@ -13,14 +14,22 @@ namespace SignaturePadMaui
             BindingContext = this;
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e) // One option to get the button is saved
         {
-
+            var stream = await DrawingView.GetImageStream(Lines,
+                new Size(400, 200), Colors.Gray);
+            drawingImage.Source = ImageSource.FromStream(() => stream);
         }
 
         private void Button_Clicked_1(object sender, EventArgs e)
         {
             Lines.Clear();
+        }
+
+        private async void DrawingView_DrawingLineCompleted(object sender, DrawingLineCompletedEventArgs e) // One option to get the image after every line
+        {
+            //var stream = await e.LastDrawingLine.GetImageStream(400, 200, Colors.Gray.AsPaint());
+            //drawingImage.Source = ImageSource.FromStream(() => stream);
         }
     }
 
